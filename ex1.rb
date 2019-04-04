ff0ba08adef33afe65410e2ab95e7d2552c092c1
shipra <shipra0ag@gmail.com>
2019-04-04 16:19:20 +0530

    first

---
+++ ex1.rb
@@ -0,0 +1,40 @@
+# puts "Hello World!."
+# puts "Hello Again."
+# puts "I like typing this."
+# puts "This is fun."
+# puts "Yay! Printing."
+# puts "I \'d much rather you \'not\'."
+
+# puts "Hello World!."
+# puts "Hello Again."
+# puts "I like typing this."
+# puts "This is fun."
+# puts "Yay! Printing."
+# puts "I \'d much rather you \'not\'."
+# puts "hello hello."
+
+
+# puts "which line you want to see from 6 line"
+# line = gets.chomp.to_i
+# puts line
+# case line
+# 	when 1 
+# 	 puts "Hello World!."
+# 	when 2 
+# 		puts "Hello Again."
+# 	when 3
+# 		puts "I like typing this."
+# 	when 4  
+# 		puts "This is fun."
+# 	when 5 
+# 		puts "Yay! Printing."
+# 	when 6
+# 		puts "I \'d much rather you \'not\'."
+# 	else
+# 		puts "#{line} not present"
+# end
+
+
+
+
+
